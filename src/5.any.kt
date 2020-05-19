97bc93f06ecceee14f14fe71edd2461ed055ec37
Ricardo Champa <rchampa.san@gmail.com>
2020-05-19 21:35:08 +0200

    Tutorial Básico 3: tipo de dato Any

--- src/5.any.kt
+++ src/5.any.kt
@@ -1,3 +1,5 @@
+import java.awt.Color
+
 // El tipo Any es lo que Object a Java
 // Any es el supertipo de todos los tipos en Kotlin
 fun main() {
@@ -5,14 +7,16 @@ fun main() {
     //1. crea distintas variables de tipo Any
     val x: Any = 2
     val nombre: Any = "Antonio"
-
+    val estatura: Any = 1.7
 
     //2. crea distintas variables de tipo Any?
     val y: Any?= 2
     val apellidos: Any? = null
 
+    val numeros = listOf<Any>(15, "20", 30.5, "cien", false)
+
     //En definitiva Object es la super clase de todas las clases en Java, mientras que
-    //Any es la super clases de todas las clases no opcionales en kotlin
+    //Any es la super clases de todas las clases NO OPCIONALES en kotlin
     //Any? si queremos también usar el valor null
 
 }
